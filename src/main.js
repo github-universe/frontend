f481320c817b8be394db34e6a5b223a711950440
陆晨尧 <lu.c.y@qq.com>
2018-05-24 10:46:50 +0800

    build env

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Toastr from 'vue-toastr'
+import 'vue-toastr/src/vue-toastr.scss'
 
 Vue.config.productionTip = false
+
+Vue.use(Toastr)
 
 new Vue({
   render: h => h(App)
